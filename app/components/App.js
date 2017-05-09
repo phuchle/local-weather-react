987e1d83aaeb130033b91b6d9d392f825e63207c
Phuc Le <le.phuc.h@gmail.com>
2017-05-08 21:22:00 -0700

    Add navbar UI.

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Home from './Home';
+import NavBar from './NavBar';
 
 class App extends React.Component {
   render() {
     return (
-      <Home />
+      <div className='container' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+        <NavBar />
+        <Home />
+      </div>
     )
   }
 }
--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div className='container' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+      <div className='home'>
         <p className='heading'>Enter a City and State</p>
         <input type='text' placeholder='San Jose, CA' />
         <button className='button'>Get Weather</button>
